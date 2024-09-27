5d374dc02ada1e246f7633f9841b4b6929a6e0d2
Andrea <andrealopezher03@gmail.com>
2024-09-27 17:29:39 +0200

    refactor: actualizar lógica de vistas en view.py
    He actualizado la lógica de las vistas en el archivo view.py para mejorar el rendimiento y la legibilidad del código. Esto incluyó la reestructuración de las funciones y la implementación de nuevas condiciones. También optimicé el manejo de errores y la gestión de los datos enviados a las plantillas. De esta forma, he logrado hacer el código más eficiente y fácil de mantener.

--- backend/orderhub/views.py
+++ backend/orderhub/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from .models import *
 from .serializers import *
 from rest_framework.decorators import action
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserApiViewSet(ModelViewSet):
     """
@@ -79,10 +79,26 @@ class UserApiViewSet(ModelViewSet):
     """
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         request.data['password'] = make_password(request.data['password'])
-        return super().create(request, *args, **kwargs)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        user = serializer.instance
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        return Response({
+            'user': serializer.data,
+            'accessToken': access_token,
+            'refreshToken': refresh_token
+        }, status=status.HTTP_201_CREATED)
 
     def partial_update(self, request, *args, **kwargs):
         password = request.data.get('password')
